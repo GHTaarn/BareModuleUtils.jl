2085a570f63456f99e15bb83ca5e022b2bc4d58b
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2025-05-08 16:48:25 +0200

    First working version

---
+++ src/BareModuleUtils.jl
@@ -0,0 +1,50 @@
+module BareModuleUtils
+
+export generate_symbols, @init_without
+
+function generate_symbols(m::Module; eval::Bool=true, include::Bool=true, baseskip=Symbol[])
+    eval && generate_eval(m)
+    include && generate_include(m)
+    for sym in filter(∉(baseskip), names(Base))
+        @eval m using Base: $(sym)
+    end
+end
+
+function import_from_base(m::Module, syms...) # TODO: make sure that syms only contains Symbols
+    for sym in syms
+        @eval m import Base: $(sym)
+    end
+end
+
+function using_from_base(m::Module, syms...) # TODO: make sure that syms only contains Symbols
+    for sym in syms
+        @eval m using Base: $(sym)
+    end
+end
+
+function generate_eval(m::Module)
+    @eval m eval(x) = Core.eval(m, x)
+end
+
+function generate_include(m::Module)
+    @eval m include(x) = Base.include(m, x)
+end
+
+macro generate_eval()
+    return :(generate_eval($(__module__)))
+end
+
+macro generate_include()
+    return :(generate_include($(__module__)))
+end
+
+macro import_from_base(syms...)
+    return :(import_from_base($(__module__), $(syms...)))
+end
+
+macro init_without(syms...)
+    return :(generate_symbols($(__module__); baseskip=vcat($(syms...))))
+end
+
+end
+
